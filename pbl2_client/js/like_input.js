7ba15b3068ebbab4b3c72fed1b32026ccb1b08cd
M-Sano <M-Sano@M-Sano-VAIO>
2014-10-31 11:38:28 +0900

    #5, 8, 9, 12, 13 いろいろ コード移植

---
+++ pbl2_client/js/like_input.js
@@ -0,0 +1,9 @@
+//var endpoint = 'http://192.168.100.10:8080/alpaca_c0/api';
+var endpoint = 'http://localhost:8080/alpaca/api';
+
+$('#like_button').click(function() {
+	$.ajax({
+		type : 'GET',
+		url : endpoint + '/like',
+	});
+});
