88f977d467c3b99bfd11d4ac7001a040802eaf8c
m-sano <m-sano@ist.osaka-u.ac.jp>
2014-11-02 16:49:55 +0900

    #13 like_graph.js_likeグラフ APIアドレスをデプロイ用に変更

--- pbl2_client/js/like_graph.js
+++ pbl2_client/js/like_graph.js
@@ -1,5 +1,6 @@
 //var endpoint = 'http://192.168.100.10:8080/alpaca_c0/api';
-var endpoint = 'http://localhost:8080/alpaca/api';
+//var endpoint = 'http://localhost:8080/alpaca/api';
+var endpoint = 'http://cloudspiral8.ddns.net/vicugna/api';
 
 google.load("visualization", "1", {packages:["corechart"]});
 
