94411907e671b141abfc987bf8fd0711cade30a4
nishi <s155032@center.wakayama-u.ac.jp>
2014-11-02 13:03:52 +0900

    コメント一覧js

--- pbl2_client/js/comment_list.js
+++ pbl2_client/js/comment_list.js
@@ -1,5 +1,5 @@
 //var endpoint = 'http://192.168.100.10:8080/alpaca_c0/api';
-var endpoint = 'http://localhost:8080/alpaca/api';
+var endpoint = 'http://cloudspiral8.ddns.net/vicugna/api';
 
 var l = window.setInterval(function() {
 	$.ajax({
