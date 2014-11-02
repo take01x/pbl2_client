03a30e9d6924fb21c6ce2a3385a498908e8ff855
nishi <s155032@center.wakayama-u.ac.jp>
2014-11-02 13:03:32 +0900

    コメント入力js

--- pbl2_client/js/comment_input.js
+++ pbl2_client/js/comment_input.js
@@ -1,6 +1,5 @@
 //var endpoint = 'http://192.168.100.10:8080/alpaca_c0/api';
-var endpoint = 'http://localhost:8080/alpaca/api';
-
+var endpoint = 'http://cloudspiral8.ddns.net/vicugna/api';
 $('#submit').click(function() {
 	var message = $('#message').val();
 	$.ajax({
